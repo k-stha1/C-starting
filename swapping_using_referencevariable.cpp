db2703f00056c120878b228a65662657b561c107
k-stha1 <kritishshrestha123@gmail.com>
2024-08-01 22:43:42 +0545

    Swapping the value of two varible using Reference varaible

--- swapping_using_referencevariable.cpp
+++ swapping_using_referencevariable.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
+/* Putting the variable in private access modifier*/
+/* #include <iostream>
 using namespace std;
 
-
-/* Putting the variable in private access modifier*/
-class Swap{
+ class Swap{
     private:
     int x,y,t;
 
@@ -38,3 +37,49 @@ int main(){
 
     return 0;
 }
+*/
+
+/*Putting the acces modfier public*/
+
+
+#include <iostream>
+using namespace std;
+
+ class Swap{
+    public:
+    int x,y,t;
+
+    void getNum(){
+        cout <<"Enter two numbers" << endl;
+        cin >> x >> y;
+        }
+
+    void swapping(int &aref,int &bref){
+        t=aref;
+        aref=bref;
+        bref=t;
+       }
+
+    void display(){
+        cout << "Numbers after swapping" << endl;
+        cout << x << " " << y << endl;
+    }
+
+};
+
+int main(){
+    Swap p1;
+    p1.getNum();
+    p1.swapping(p1.x,p1.y);
+    p1.display();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
