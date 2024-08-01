581c1b5b73200cf4bb6cdd17a769997d024a4aa1
k-stha1 <kritishshrestha123@gmail.com>
2024-08-01 22:11:59 +0545

    Adding a file

---
+++ swapping_using_referencevariable.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+/* Putting the variable in private access modifier*/
+class Swap{
+    private:
+    int x,y,t;
+
+    public:
+
+    void getNum(){
+        cout <<"Enter two numbers" << endl;
+        cin >> x >> y;
+        }
+
+    void swapping(int &aref,int &bref){
+        t=aref;
+        aref=bref;
+        bref=aref;
+       }
+
+    void display(){
+        cout << "Numbers after swapping" << endl;
+        cout << x << " " << y << endl;
+    }
+
+    void swapper(){
+        swapping (x,y);
+    }
+};
+
+int main(){
+    Swap p1;
+    p1.getNum();
+    p1.swapper();
+    p1.display();
+
+    return 0;
+}
