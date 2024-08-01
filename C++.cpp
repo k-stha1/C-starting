1856d9d7d73109b1c2bebd0cf349610116877ebf
k-stha1 <kritishshrestha123@gmail.com>
2024-08-01 21:57:29 +0545

    modifying both

--- C++.cpp
+++ C++.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 int main (){
     cout << "Hello World" << endl;
+    cout << "Hello Kritish and everyone" << endl;
+    
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # C-starting
 This is me learning to use github and C++
 <br> I am changing from vs code.
+<br> changing again
