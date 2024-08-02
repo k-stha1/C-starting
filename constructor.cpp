251258e528b206c66e2c58c74157bd3870a4ac2f
k-stha1 <kritishshrestha123@gmail.com>
2024-08-02 19:15:13 +0545

    Use on constructor

--- constructor.cpp
+++ constructor.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 
 class bankdetail {
@@ -30,4 +30,42 @@ int main() {
     b1.display();
     b2.display();
     return 0;
+}
+*/ 
+
+// Using copy constructor 
+#include <iostream>
+using namespace std;
+
+class book{
+    public:
+    int id;
+    string name;
+
+    book(int book_id=0,string book_name="No Name");
+
+    void display(){
+        cout << "Book id" << id << "Book name" << name << endl;
+    }
+    book(const book &other){
+        id=other.id;
+        name=other.name;
+    }
+};
+
+book::book(int book_id,string book_name){
+    id=book_id;
+    name=book_name;
+}
+
+int main(){
+    book b1;
+    b1.display();
+    book b2(1,"The last of us");
+    b2.display();
+    book b3 = b2;
+    b3.display();
+
+    return 0;
+
 }
