6ba259dbdc6ff816199a3a0a65a7d0aa59a857d2
k-stha1 <kritishshrestha123@gmail.com>
2024-08-04 13:03:11 +0545

    Greatest among two give time in hours and minutes

---
+++ Greatest.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class Time{
+	private:
+	int HH;
+	int MM;
+
+	public:
+
+	Time(int hours=0,int minutes =0){
+		HH = hours;
+		MM = minutes;
+	}
+
+	void displayTime(){
+		cout << HH << " Hours and " << MM << " minutes" << endl; 
+	}
+
+	bool operator >(Time& t2){
+		if (HH > t2.HH){
+			return true;
+		}
+		else if (HH == t2.HH && MM > t2.MM) {
+        return true;
+    }
+    return false;
+		
+	}
+
+	void displayResult( int t3){
+	cout << "if t1 > t2 result is 1" << endl;
+	cout << "if t1 < t2 result is 0" << endl;
+
+	cout << "Is t1 > t2?" << t3 << endl;
+
+	}
+
+	Time (const Time& other){
+		HH=other.HH;
+		MM= other.MM;
+
+
+	}
+
+};
+
+int main(){
+	Time t1(1,20);
+	Time t2(1,40);
+	Time t3(t1);
+	
+	cout << "Time 1 is:" << endl;
+	t1.displayTime();
+	cout << "Time 2 is:" << endl;
+	t2.displayTime();
+	cout << "Time 3 is:" << endl;
+	t3.displayTime();
+
+	if(t1 > t2 ){
+		cout << "Time t1 is greater then time t2." << endl;
+		}
+		else{
+			cout << "Time t2 is greater than time t1." << endl;
+		}
+
+	if (t3>t2){
+		cout << "Time t3 is greater then time t2." << endl;
+		}
+		else{
+			cout << "Time t2 is greater than time t3." << endl;
+
+	}
+	
+
+	return 0;
+
+}
