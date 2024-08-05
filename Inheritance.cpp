634f756bb5c47331cdc836ecd547f6105cc6dd4d
k-stha1 <kritishshrestha123@gmail.com>
2024-08-05 21:24:33 +0545

    Single inheritance simple program

---
+++ Inheritance.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Animal{
+    public:
+    void display(){
+        cout << "The animal can walk" << endl;
+    }
+};
+
+class Mammal: public Animal{
+    public:
+
+    void feature(){
+    display();
+    cout << "Mammal dont lay egg" ;
+    }
+};
+
+int main(){
+    Mammal human;
+    human.feature();
+    human.display();
+    
+     return 0;
+
+}
