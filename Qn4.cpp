0fa009faf793576567bd20c86af3f4f9fa1f327d
k-stha1 <kritishshrestha123@gmail.com>
2024-08-27 06:42:01 +0545

    Jan 2023 qn number10b

---
+++ Qn4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>>
+using namespace std;
+
+    void read(double a, double b){
+        cout << "Enter two double numbers" << endl;
+        cin >> a >> b;
+    }
+
+    void calc (double a, double b){
+        try {
+            if (b==0){
+                throw 0;
+            }
+            double c = a/b;
+            cout << "Result: " << c;
+        }
+        catch ( int num){
+            cout << "Cannot divide by " << num << endl;
+
+        }
+    }
+int main(){
+    double a,b;
+    read (a,b);
+    calc (a,b);
+    return 0;
+
+}
+
